9578f7a1a91717d360671e916de9de214d48e4bd
Grant Cox <zygh0st@no.spam.ho>
2023-12-28 03:43:37 -0600

    Fixes

--- src/EventListeners.ts
+++ src/EventListeners.ts
@@ -12,7 +12,7 @@ import { antRepeat, sacrificeAnts, buyAntProducers, updateAntDescription, antUpg
 import { buyCubeUpgrades, cubeUpgradeDesc } from './Cubes'
 import { buyPlatonicUpgrades, createPlatonicDescription } from './Platonic'
 import { corruptionCleanseConfirm, corruptionDisplay } from './Corruptions'
-import { editData, exportSynergism, instantCheat, instantFix, cheatModSingularity, cheatAddSingularity, cheatSubSingularity, updateSaveString, promocodes, promocodesPrompt, promocodesInfo, importSynergism, resetGame, reloadDeleteGame, importData, addCodeAvailableUses, importDataFromText } from './ImportExport'
+import { editData, exportSynergism, instantCheat, instantFix, cheatModSingularity, updateSaveString, promocodes, promocodesPrompt, promocodesInfo, importSynergism, resetGame, reloadDeleteGame, importData, addCodeAvailableUses, importDataFromText } from './ImportExport'
 import { resetHistoryTogglePerSecond } from './History'
 import { resetShopUpgrades, shopDescriptions, buyShopUpgrades, useConsumable, shopData, shopUpgradeTypes } from './Shop'
 import { Globals as G } from './Variables'
@@ -566,8 +566,8 @@ export const generateEventHandlers = () => {
   DOMCacheGetOrSet('editGameButton').addEventListener('click', () => editData())
   DOMCacheGetOrSet('instantCheatButton').addEventListener('click', () => instantCheat())
   DOMCacheGetOrSet('instantFixButton').addEventListener('click', () => instantFix())
-  DOMCacheGetOrSet('cheatAddSingularity').addEventListener('click', () => cheatAddSingularity())
-  DOMCacheGetOrSet('cheatSubSingularity').addEventListener('click', () => cheatSubSingularity())
+  //DOMCacheGetOrSet('cheatAddSingularity').addEventListener('click', () => cheatAddSingularity())
+  //DOMCacheGetOrSet('cheatSubSingularity').addEventListener('click', () => cheatSubSingularity())
   DOMCacheGetOrSet('cheatModSingularity').addEventListener('click', () => cheatModSingularity())
   DOMCacheGetOrSet('saveStringInput').addEventListener('blur', e => updateSaveString(e.target as HTMLInputElement))
   DOMCacheGetOrSet('savegame').addEventListener('click', () => saveSynergy(true))
